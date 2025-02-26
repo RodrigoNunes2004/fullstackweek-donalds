cfb9e2c9c770655169dad9accdc5d3961b265077
RodrigoNunes2004 <rodrigo_samambaia@hotmail.com>
2025-02-26 15:24:34 +1300

    Feat add categories.tsx, products.tsx, header.tsx in menu folder

--- src/app/products/page.tsx
+++
@@ -1,9 +0,0 @@
-const ProductdPage = () => {
-    return ( 
-        <div className="p-5 border border-red-500 rounded-xl">
-    <h1 className="text-red-500">Products</h1>
-    </div>
- );
-}
- 
-export default ProductdPage;
