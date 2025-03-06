116b57e8f0be6cf613c9e3cd15cd9ed8fd79c9f7
RodrigoNunes2004 <rodrigo_samambaia@hotmail.com>
2025-03-06 23:56:30 +1300

    Feat: add initial context/ cart, components/ cart-sheet and ensure same product can be added more than once to cart

--- src/app/[slug]/menu/[productId]/components/product-details.tsx
+++ src/app/[slug]/menu/[productId]/components/product-details.tsx
@@ -3,15 +3,16 @@
 import { Prisma } from "@prisma/client";
 import { ChefHatIcon, ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
 import Image from "next/image";
-import { useState } from "react";
+import { useContext, useState } from "react";
 
 import { Button } from "@/components/ui/button";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { formatCurrency } from "@/helpers/format-currency";
-/*
+
 import CartSheet from "../../components/cart-sheet";
 import { CartContext } from "../../contexts/cart";
-*/
+
+
 interface ProductDetailsProps {
   product: Prisma.ProductGetPayload<{
     include: {
@@ -26,7 +27,7 @@ interface ProductDetailsProps {
 }
 
 const ProductDetails = ({ product }: ProductDetailsProps) => {
-  //const { toggleCart, addProduct } = useContext(CartContext);
+  const { toggleCart, addProduct } = useContext(CartContext);
   const [quantity, setQuantity] = useState<number>(1);
   const handleDecreaseQuantity = () => {
     setQuantity((prev) => {
@@ -39,7 +40,13 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
   const handleIncreaseQuantity = () => {
     setQuantity((prev) => prev + 1);
   };
- 
+  const handleAddToCart = () => {
+    addProduct({
+      ...product,
+      quantity,
+    });
+    toggleCart();
+  };
   return (
     <>
       <div className="relative z-50 mt-[-1.5rem] flex flex-auto flex-col overflow-hidden rounded-t-3xl p-5">
@@ -109,11 +116,11 @@ const ProductDetails = ({ product }: ProductDetailsProps) => {
           </ScrollArea>
         </div>
 
-        <Button className="w-full rounded-full" >
+        <Button className="w-full rounded-full" onClick={handleAddToCart}>
           Adicionar à sacola
         </Button>
       </div>
-      
+      <CartSheet />
     </>
   );
 };
